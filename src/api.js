473eba56613178c85b98a399b32faafee74d3af4
Mevlüt Yıldırım <mevlut.yildirim84@erzurum.edu.tr>
2021-01-15 23:16:20 +0300

    create

---
+++ src/api.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const serverless = require('serverless-http');
+
+const app = express();
+
+const router = express.Router();
+
+router.get('/test',(req,res)=>{
+    res.json({
+        'hello' :'test'
+    });
+});
+
+app.use('/.netlify/functions/api',router);
+
+
+
+
+module.exports.handler = serverless(app);
+
