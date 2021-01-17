a20c0c38387a283fda6404da4661a9ff5eae8c12
Mevlüt Yıldırım <mevlut.yildirim84@erzurum.edu.tr>
2021-01-17 21:53:01 +0300

    up

--- routes/intern.js
+++ routes/intern.js
@@ -8,7 +8,7 @@ const Intern = require('../models/Intern');
 
 router.post('/iste',(req,res,next)=>{
 
-    const intern = new Intern(event.body);
+    const intern = new Intern(req.body);
 
     const promise = intern.save();
 
